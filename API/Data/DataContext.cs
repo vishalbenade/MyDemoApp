d978ec71bad8975d84d4e0958720df520b04e2a2
vishalbenade <vishu95.vb@gmail.com>
2021-04-07 19:34:35 +0530

    end of session 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<AppUser> Users{get;set;}
+    }
+}
